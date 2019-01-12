0cd118131f5e0f98523b41e537ea0b8efe4cdad8
iuoon <710731936@qq.com>
2019-01-12 18:27:51 +0800

    服务通讯字节修改

--- rbgate/main/Client.go
+++ rbgate/main/Client.go
@@ -28,16 +28,16 @@ func sendMessage(conn *net.TCPConn) {
 	//time.Sleep(1 * time.Microsecond)
 	//content:="{\"code\":\"asdjkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkweqweopqweqopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopopop\",\"msg\":\"weweeweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee\"}"
 	content := "{\"cmd\":\"Login\",\"token\":\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDgwNDcyODcsImlhdCI6MTU0Njc1MTI4NywiaXNzIjoidHEuaXVvb24uY29tIiwibWFjIjoiIiwidWlkIjoiXHUwMDAxIn0.MKFz6VTLbQmyXMtcoO1SQ2e092yys5tSJtqO2abBe0I\",\"requestId\":\"1\",\"mac\":\"\"}"
-	b, _ := base.EncodeHead2Byte(string(content))
+	b, _ := base.EncodeHead4Byte(string(content))
 	conn.Write(b)
     time.Sleep(time.Second*time.Duration(1))
 	content = "{\"cmd\":\"LoginRole\",\"roleId\":1,\"requestId\":\"1\",\"mac\":\"\"}"
-	b, _ = base.EncodeHead2Byte(string(content))
+	b, _ = base.EncodeHead4Byte(string(content))
 	conn.Write(b)
 
 	time.Sleep(1 * time.Second)
 	content = "{\"cmd\":\"GetAroundPlayers\",\"roleId\":1,\"requestId\":\"1\",\"mac\":\"\"}"
-	b, _ = base.EncodeHead2Byte(string(content))
+	b, _ = base.EncodeHead4Byte(string(content))
 	conn.Write(b)
 
 
